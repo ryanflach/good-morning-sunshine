3340743504b2cc1553bea85a494344e3775fa813
Ryan Flach <ryanflach@users.noreply.github.com>
2016-10-25 15:23:02 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src="gms_logo.png" alt='logo'></img><br />
+        Good Morning, Sunshine?
+      </div>
+    );
+  }
+}
+
+export default App;

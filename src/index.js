043700042b97dd8a831521128cfb30a13cad77ef
Ryan Flach <ryanflach@users.noreply.github.com>
2016-10-25 20:40:55 -0600

    [WIP] Add Firebase - environment variables are stored securely and saved on Heroku dashboard

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
 import App from './App';
 import './index.css';
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MS_ID
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
